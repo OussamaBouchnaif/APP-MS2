faa823414bf51cfbd732b0d0f05422c502357409
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-07-11 22:05:01 +0200

    first commit

---
+++ MS2Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MS2Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
