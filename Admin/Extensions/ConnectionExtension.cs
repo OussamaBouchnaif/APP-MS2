f2624b24c11149e20bc359b0a9250aaa3779561e
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-07-14 12:36:57 +0200

    update extensions

--- Admin/Extensions/ConnectionExtension.cs
+++ Admin/Extensions/ConnectionExtension.cs
@@ -1,6 +1,15 @@
-﻿namespace Admin.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using MS2Api.Data;
+
+namespace Admin.Extensions
 {
-    public class ConnectionExtension
+    public static class ConnectionExtension
     {
+        public static IServiceCollection AddGestionCommandesContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<MyContext>(o =>
+            o.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb")));
+            return builder.Services;
+        }
     }
 }
--- Admin/Extensions/ServiceInjectionExtensions.cs
+++ Admin/Extensions/ServiceInjectionExtensions.cs
@@ -1,6 +1,21 @@
-﻿namespace Admin.Extensions
+﻿using Admin.Repository;
+using Admin.Service.Contract;
+using Admin.Service;
+
+namespace Admin.Extensions
 {
-    public class ServiceInjectionExtensions
+    public static class ServiceInjectionExtensions
     {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            // Ajouter le repository générique
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            // Ajouter le service des bénéficiaires
+            services.AddScoped<IBeneficiaryService, BenificierService>();
+
+            // Ajouter le mapper des bénéficiaires
+            //services.AddScoped<IBenificierMapper, BenificierMapper>();
+        }
     }
 }
