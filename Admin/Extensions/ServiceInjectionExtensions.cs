6bfdf23886113e81265b6195c6d39ae73de2df7c
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-07-14 12:34:52 +0200

    add extensions

---
+++ Admin/Extensions/ServiceInjectionExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Admin.Extensions
+{
+    public class ServiceInjectionExtensions
+    {
+    }
+}
--- Admin/Program.cs
+++ Admin/Program.cs
@@ -1,9 +1,5 @@
+
 using Admin.Extensions;
-using Admin.Repository;
-using Admin.Service.Contract;
-using Microsoft.EntityFrameworkCore;
-using MS2Api.Data;
-using Admin.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,15 +8,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Configuration.AddCustomConfiguration(builder.Environment);
 
-builder.Services.AddDbContext<MyContext>(option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb"))
-    );
+builder.AddGestionCommandesContext();
 
-// Ajouter le repository générique
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-// Ajouter le service des bénéficiaires
-builder.Services.AddScoped<IBeneficiaryService, Admin.Service.BenificierService>();
+builder.Services.AddCustomServices();
 
 var app = builder.Build();
 
@@ -42,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Benificier}/{action=Index}/{id?}");
+    pattern: "{controller=Benificier}/{action=Add}/{id?}");
 
 app.Run();
