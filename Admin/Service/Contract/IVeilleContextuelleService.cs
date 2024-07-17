7a2b7d22c045d0f6e9bcccb1f0f582ee06ce3a15
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2024-07-17 16:00:31 +0100

    add veille technologique (#21)
    * configuration d'un middleware des erreurs
    
    configuration d'un middleware des erreurs pour la gestion des erreurs on mode poroduction ( 404 et 500 )
    
    * add icon app
    
    add icon app
    
    * add login page with Controller
    
    add login page with Controller  && grid js toDisplay data for any model
    
    * add identitiy and configuration for all users
    
    add identitiy and configuration for all users
    
    * version 1
    
    version stable
    
    * Veille strategique
    
    Veille strategique
    
    * add veille technonologique
    
    add veille technonologique

---
+++ Admin/Service/Contract/IVeilleContextuelleService.cs
@@ -0,0 +1,12 @@
+﻿using Admin.Models;
+using Admin.ViewModel;
+
+namespace Admin.Service.Contract
+{
+    public interface IVeilleContextuelleService
+    {
+        IEnumerable<VeilleContextuelle> GetAllVeilles();
+
+        void AddVeille(VeilleContextuelleViewModel model, int[] sourceInformation, int[] typeMigrants, int[] nationalites);
+    }
+}
---
+++ Admin/Service/VeilleContextuelleService.cs
@@ -0,0 +1,37 @@
+﻿using Admin.Flags;
+using Admin.Mapper.Contract;
+using Admin.Models;
+using Admin.Repository;
+using Admin.Service.Contract;
+using Admin.ViewModel;
+
+namespace Admin.Service
+{
+    public class VeilleContextuelleService : IVeilleContextuelleService
+    {
+        private readonly IRepository<VeilleContextuelle> _veilleContextuelleRepository;
+        private readonly IVeilleContextuelleMapper _mapper;
+
+        public VeilleContextuelleService(IRepository<VeilleContextuelle> veilleContextuelleRepository, IVeilleContextuelleMapper mapper)
+        {
+            this._veilleContextuelleRepository = veilleContextuelleRepository;
+            _mapper = mapper;
+        }
+
+        public void AddVeille(VeilleContextuelleViewModel model, int[] sourceInformation, int[] typeMigrants, int[] nationalites)
+        {
+            var veille = _mapper.MapToEntity(model);
+            veille.SourceInformation = (SourceInformation)sourceInformation.Aggregate(0, (current, value) => current | value);
+            veille.TypeMigrants = (TypeMigrants)typeMigrants.Aggregate(0, (current, value) => current | value);
+            veille.Nationalites = (Nationalites)nationalites.Aggregate(0, (current, value) => current | value);
+
+            _veilleContextuelleRepository.Insert(veille);
+            _veilleContextuelleRepository.SaveChanges();
+        }
+
+        public IEnumerable<VeilleContextuelle> GetAllVeilles()
+        {
+            return _veilleContextuelleRepository.GetAll().ToList();
+        }
+    }
+}
