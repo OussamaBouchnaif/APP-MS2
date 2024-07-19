f6a5ec53ee73995b03d2467059cf4a0f8a9166ed
HASHAS Jad el mawla <92980923+jadhashas@users.noreply.github.com>
2024-07-19 20:15:41 +0100

    configuration du VC (#33)
    * configuration d'un middleware des erreurs
    
    configuration d'un middleware des erreurs pour la gestion des erreurs on mode poroduction ( 404 et 500 )
    
    * add icon app
    
    add icon app
    
    * add login page with Controller
    
    add login page with Controller  && grid js toDisplay data for any model
    
    * add identitiy and configuration for all users
    
    add identitiy and configuration for all users
    
    * version 1
    
    version stable
    
    * Veille strategique
    
    Veille strategique
    
    * add veille technonologique
    
    add veille technonologique
    
    * finalisation du veille strategique
    
    finalisation du veille strategique
    
    * add count default to veille technologique
    
    add count default to veille technologique
    
    * add statistique to veille contextuelle
    
    add statistique to veille contextuelle
    
    * finalisation statistique
    
    finalisation statistique
    
    * finalisation du veille contextuelle
    
    finalisation du veille contextuelle
    
    * continuation de configuration du VC
    
    continuation de configuration du VC

--- Admin/Service/VeilleContextuelleService.cs
+++ Admin/Service/VeilleContextuelleService.cs
@@ -1,116 +1,100 @@
 using Admin.Enums;
+using Admin.Flags;
+using Admin.Mapper;
 using Admin.Mapper.Contract;
+using Admin.Models;
 using Admin.Repository;
-using Admin.Service;
 using Admin.Service.Contract;
 using Admin.ViewModel;
-using Microsoft.AspNetCore.Mvc;
-using MS2Api.Model;
+using Microsoft.EntityFrameworkCore;
 
-namespace Admin.Controllers
+namespace Admin.Service
 {
-    [ServiceFilter(typeof(AuthenticationFilter))]
-    public class VeilleContextuelleController : Controller
+    public class VeilleContextuelleService : IVeilleContextuelleService
     {
-        private readonly IVeilleContextuelleService veilleContextuelleService;
-        private readonly IHttpContextAccessor httpContextAccessor;
-        private readonly IVeilleContextuelleMapper veilleContextuelleMapper;
+        private readonly IRepository<VeilleContextuelle> _veilleContextuelleRepository;
+        private readonly IVeilleContextuelleMapper _mapper;
 
-        public VeilleContextuelleController(IVeilleContextuelleService veilleContextuelleService, IHttpContextAccessor httpContextAccessor, IVeilleContextuelleMapper veilleContextuelleMapper)
+        public VeilleContextuelleService(IRepository<VeilleContextuelle> veilleContextuelleRepository, IVeilleContextuelleMapper mapper)
         {
-            this.veilleContextuelleService = veilleContextuelleService;
-            this.httpContextAccessor = httpContextAccessor;
-            this.veilleContextuelleMapper = veilleContextuelleMapper;
+            this._veilleContextuelleRepository = veilleContextuelleRepository;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public void AddVeille(VeilleContextuelleViewModel model, int[] sourceInformation, int[] typeMigrants, int[] nationalites)
         {
-            var veilleList = veilleContextuelleService.GetFilteredVeilles();
-            return View(veilleList);
+            var veille = _mapper.MapToEntity(model);
+            veille.SourceInformation = (SourceInformation)sourceInformation.Aggregate(0, (current, value) => current | value);
+            veille.TypeMigrants = (TypeMigrants)typeMigrants.Aggregate(0, (current, value) => current | value);
+            veille.Nationalites = (Nationalites)nationalites.Aggregate(0, (current, value) => current | value);
+
+            _veilleContextuelleRepository.Insert(veille);
+            _veilleContextuelleRepository.SaveChanges();
         }
 
-        [HttpGet]
-        public IActionResult Create()
+        public IEnumerable<VeilleContextuelleViewModel> GetFilteredVeilles()
         {
-            var user = httpContextAccessor.HttpContext.Session.GetObjectFromJson<Utilisateur>("User");
-            var model = new VeilleContextuelleViewModel
-            {
-                UtilisateurId = user.Id
-            };
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult Create(VeilleContextuelleViewModel model, int[] SourceInformation, int[] TypeMigrants, int[] Nationalites)
-        {
-            if (ModelState.IsValid)
-            {
-                veilleContextuelleService.AddVeille(model, SourceInformation, TypeMigrants, Nationalites);
-                return Ok(new { message = "La veille contextuelle a été ajoutée avec succès." });
-            }
-            var user = httpContextAccessor.HttpContext.Session.GetObjectFromJson<Utilisateur>("User");
-            model.UtilisateurId = user.Id;
-            return BadRequest(ModelState);
-        }
-
-        [HttpPost]
-        public IActionResult UpdateStatus(int veilleId, VerificationStatus status)
-        {
-            veilleContextuelleService.UpdateVerificationStatus(veilleId, status);
-            return Ok(new { message = "Le statut a été mis à jour avec succès." });
-        }
-
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            var veille = veilleContextuelleService.GetVeilleById(id);
-            if (veille == null)
-            {
-                return NotFound();
-            }
-            var veilleVM = veilleContextuelleMapper.MapToViewModel(veille);
-            veilleVM.AgentMS2 = veille.Utilisateur != null ? veille.Utilisateur.Nom + ' ' + veille.Utilisateur.Prenom : "N/A";
-
-            var topNationalities = veilleContextuelleService.GetTopNationalities(veille);
-            ViewBag.TopNationalities = topNationalities;
-
-            return View(veilleVM);
-        }
-
-        [HttpGet]
-        public IActionResult GetStatistiques(int id)
-        {
-            var veille = veilleContextuelleService.GetVeilleById(id);
-            if (veille == null)
-            {
-                return NotFound();
-            }
-
-            var data = new
-            {
-                GenderData = new
+            return _veilleContextuelleRepository.GetAll()
+                .Select(v => new VeilleContextuelleViewModel
                 {
-                    Hommes = veille.NombreHommes ?? 0,
-                    Femmes = veille.NombreFemmes ?? 0,
-                    Enfants = veille.NombreEnfants ?? 0,
-                    MENA = veille.NombreMENA ?? 0
-                },
-                NationalityData = new
-                {
-                    Soudan = veille.NombreSoudan ?? 0,
-                    SudSoudan = veille.NombreSudsoudan ?? 0,
-                    Guinee = veille.NombreGuinee ?? 0,
-                    Cameroun = veille.NombreCameroun ?? 0,
-                    CotedIvoire = veille.NombreCotedIvoire ?? 0,
-                    Mali = veille.NombreMali ?? 0,
-                    Nigeria = veille.NombreNigeria ?? 0,
-                    Senegal = veille.NombreSenegal ?? 0,
-                    RDC = veille.NombreRDC ?? 0,
-                    Autres = veille.NombreAutreNationalites ?? 0
-                },
-            };
+                    Id = v.Id,
+                    DateEvenement = v.DateEvenement,
+                    TypeEvenement = v.TypeEvenement,
+                    SourceInformation = v.SourceInformation,
+                    DetailsEvenement = v.DetailsEvenement,
+                    VerificationStatus = v.VerificationStatus,
+                    AgentMS2 = v.Utilisateur.Nom + ' ' + v.Utilisateur.Prenom,
+                }).ToList();
+        }
 
-            return Json(data);
+        public IEnumerable<VeilleContextuelle> GetAllVeilles()
+        {
+            return _veilleContextuelleRepository.GetAll().ToList();
+        }
+
+        public void UpdateVerificationStatus(int veilleId, VerificationStatus status)
+        {
+            var veille = _veilleContextuelleRepository.FindById(veilleId);
+            if (veille != null)
+            {
+                veille.VerificationStatus = status;
+                _veilleContextuelleRepository.Update(veille);
+                _veilleContextuelleRepository.SaveChanges();
+            }
+        }
+
+        public VeilleContextuelle GetVeilleById(int id)
+        {
+            var veille = _veilleContextuelleRepository.GetAll()
+            .Include(v => v.Utilisateur)
+            .FirstOrDefault(v => v.Id == id);
+
+            if (veille == null)
+                return null;
+            return veille;
+        }
+
+        public (string, int)[] GetTopNationalities(VeilleContextuelle veille)
+        {
+            var nationalities = new Dictionary<string, int>
+                {
+                    { "Soudan", veille.NombreSoudan ?? 0 },
+                    { "Sud Soudan", veille.NombreSudsoudan ?? 0 },
+                    { "Guinée", veille.NombreGuinee ?? 0 },
+                    { "Cameroun", veille.NombreCameroun ?? 0 },
+                    { "Côte d'Ivoire", veille.NombreCotedIvoire ?? 0 },
+                    { "Mali", veille.NombreMali ?? 0 },
+                    { "Nigeria", veille.NombreNigeria ?? 0 },
+                    { "Sénégal", veille.NombreSenegal ?? 0 },
+                    { "RDC", veille.NombreRDC ?? 0 },
+                    { "Autres", veille.NombreAutreNationalites ?? 0 }
+                };
+
+            return nationalities
+                .OrderByDescending(n => n.Value)
+                .Take(3)
+                .Select(n => (n.Key, n.Value))
+                .ToArray();
         }
 
         public (string, int)[] GetTopNationalities(VeilleContextuelle veille)
