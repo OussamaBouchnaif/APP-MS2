14cd4b5f79c1cc304c086377722b8cdd702eeeae
HASHAS Jad el mawla <jouj969@gmail.com>
2024-07-26 11:13:30 +0100

    add front to social folder
    add front to social folder

---
+++ Admin/Controllers/SocialController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    [ServiceFilter(typeof(AuthenticationFilter))]
+    public class SocialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddDossierSocial(int BeneficiaryId)
+        {
+            ViewData["Id"] = BeneficiaryId;
+            return View();
+        }
+    }
+}
